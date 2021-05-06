ebba84a9292c5ebd89611ca1f43ac97dbbf21aed
Esha Vats <56391290+eshavats@users.noreply.github.com>
2021-05-06 17:39:58 +0530

    add to cart menu side complete

--- FoodOrdering/views.py
+++ FoodOrdering/views.py
@@ -11,7 +11,29 @@ def home(request):
 def menu(request):
     if(request.method == "GET"):
         foods = Food.objects.all()
-    return render(request,"menu.html", {'foods': foods})
+        return render(request,"menu.html", {'foods': foods})
+    else:
+        id = request.POST["id"]
+        food = Food.objects.get(pk=id)
+
+        cart = request.session.get("cart")
+
+        if cart:
+            quantity = cart.get(id)
+
+            if quantity:
+                cart[id] = quantity+1
+            else:
+                cart[id] = 1
+        else:
+            cart = {}
+            cart[id] = 1
+        
+        request.session["cart"] = cart
+        print(cart)
+        request.session["msg"] = food.name + " " + "successfully added to cart!"
+        return redirect("/menu")
+
 
 def cart(request):
     return render(request,"cart.html")
