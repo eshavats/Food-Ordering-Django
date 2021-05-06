25fb3a351bbe0d71d3f11669310d4066baf72bd8
Esha Vats <56391290+eshavats@users.noreply.github.com>
2021-05-06 18:37:29 +0530

    insert and delete items from cart complete

--- FoodOrdering/views.py
+++ FoodOrdering/views.py
@@ -36,4 +36,30 @@ def menu(request):
 
 
 def cart(request):
-    return render(request,"cart.html")
+    if(request.method == "GET"):
+        cart = request.session.get("cart")
+        items = []
+        total = 0
+
+        if cart:
+            for id in cart:
+                itm = {}
+                food = Food.objects.get(pk=id)
+
+                itm["id"] = id
+                itm["name"] = food.name
+                itm["price"] = food.price
+                itm["quantity"] = cart[id]
+
+                total += food.price*cart[id]
+
+                items.append(itm)
+
+            return render(request, "cart.html", {'items': items, 'total': total})
+    else:
+        id = request.POST["id"]
+        cart = request.session.get("cart")
+        del cart[id]
+        request.session["cart"] = cart
+        print(request.session.get("cart"))
+        return redirect("/cart")
