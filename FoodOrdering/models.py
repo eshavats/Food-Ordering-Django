6518732926fa25a4e898b66cbbff0c34bdec60e2
Esha Vats <56391290+eshavats@users.noreply.github.com>
2021-05-06 16:29:45 +0530

    create food model and migrate

--- FoodOrdering/models.py
+++ FoodOrdering/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=50)
+    desc = models.CharField(max_length=150)
+    price = models.FloatField()
+    img = models.CharField(max_length=250)
